76304f1730f3317dcd2ec9d7d0aa380c765d33f9
Shubha1096 <74348224+Shubha1096@users.noreply.github.com>
2020-12-14 19:27:29 +0900

    commit

--- index.js
+++ index.js
@@ -4,23 +4,48 @@ var outputDiv = document.querySelector("#Output");
 
 function clickHandler() {
     var inputText = inputnum.value;
+    var myString = inputText; 
+    var array = new Array();
 
-    var i, flag = true;
-    var prime= "is prime";
-    var noprime= "not prime";
+    array = myString.split('-');
 
-    for( i = 2; i <= inputText - 1; i++) 
-                if (inputText % i == 0) { 
-                    flag = false; 
-                    break; 
-                } 
-                  
-            if (flag == true) 
-                
-                outputDiv.value = prime;
-            else
+    var newDate = (array[2]);
 
-            outputDiv.value = noprime;
+    
+
+    var isPrime = true;
+
+    // check if number is equal to 1
+    if (newDate === 1) {
+        var ex0 = "1 is neither prime nor composite number."
+        outputDiv.innerText = ex0;
     }
+    
+    // check if number is greater than 1
+    else if (newDate > 1) {
+    
+        // looping through 2 to number-1
+        for (let i = 2; i < newDate; i++) {
+            if (newDate % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    
+        if (isPrime) {
+            var ex1 =  "is a prime number"
+            outputDiv.innerText = ex1;
+        } else {
+            var ex2 = "is a not prime number"
+            outputDiv.innerText = ex2;
+        }
+    }
+    
+    // check if number is less than 1
+    else {
+        var ex3 = "The number is not a prime number."
+        outputDiv.innerText = ex3;
+    }
+}   
 
 btnverify.addEventListener("click", clickHandler);  
