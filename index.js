b3c1adebee8dc99bacaffae4814c1f6112f586da
Shubha1096 <74348224+Shubha1096@users.noreply.github.com>
2020-12-14 18:33:20 +0900

    Commit

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ var outputDiv = document.querySelector("#Output");
 function clickHandler() {
     var inputText = inputnum.value;
 
-    //if(inputText ==1){
-       // outputDiv.innertext("Ok")
-       return inputText;
+    var i, flag = true;
+    var prime= "is prime";
+    var noprime= "not prime";
+
+    for( i = 2; i <= inputText - 1; i++) 
+                if (inputText % i == 0) { 
+                    flag = false; 
+                    break; 
+                } 
+                  
+            if (flag == true) 
+                
+                outputDiv.value = prime;
+            else
+
+            outputDiv.value = noprime;
     }
 
-btnverify.addEventListener("click", clickHandler)
+btnverify.addEventListener("click", clickHandler);  
